d3a64740521b86259cf5c6ae1016412a650d7326
gustirezky <rezky.ananda18@gmail.com>
2017-04-02 15:10:27 +0800

    Posttest 5

---
+++ src/Tugas6/DefaultTabelModel.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas6;
+
+/**
+ *
+ * @author gustirezky
+ */
+class DefaultTabelModel {
+
+    void addRow(Object[] data) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    Object getDataVector() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void fireTableDataChanged() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
